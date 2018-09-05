4157a0a852f59dd7395e6fb7dde964e0d9c73d4d
estenp <estenpatrick@gmail.com>
2018-09-05 15:56:13 -0500

    changed site title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="CFI Shades" />
     <Navbar />
     <div>{children()}</div>
   </div>
