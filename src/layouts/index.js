264aa980a2eca504c104faca074ed898e16bfe1e
Patrick <estenpatrick@gmail.com>
2018-09-13 17:59:40 -0500

    added type field to blog post

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Custom Fabricating Industries" />
     <Navbar />
     <div>{children()}</div>
   </div>
