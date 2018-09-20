2ae48ea3f3cf2776f218719c5c26fcb64b4a8c12
Patrick <estenpatrick@gmail.com>
2018-09-20 11:31:42 -0500

    added responsive classes to features component

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline feature-grid">
     {gridItems.map(item => (
-      <div key={item.image} className="column is-2">
+      <div key={item.image} className="column is-2 is-one-sixth-widescren is-one-sixth-desktop">
         <section className="section">
           <a className="has-text-centered" href={item.url}>
             <img alt={item.image} src={item.image} />
