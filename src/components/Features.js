01549ecdbab7e358a302ba80827acc1cfaebe4d2
Patrick <estenpatrick@gmail.com>
2018-09-20 08:28:58 -0500

    done adding all the vendors logos and links

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline">
+  <div className="columns is-multiline feature-grid">
     {gridItems.map(item => (
-      <div key={item.image} className="column is-6">
+      <div key={item.image} className="column is-2">
         <section className="section">
           <a className="has-text-centered" href={item.url}>
             <img alt={item.image} src={item.image} />
