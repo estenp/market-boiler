0d0b7a778ffacf638471185020520afa6db3920a
Patrick <estenpatrick@gmail.com>
2018-10-10 20:18:40 -0500

    gallery modal WIP

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,16 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Modal from 'react-modal'
 
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
 
-const ImageGallery = ({ imageUrls }) => (
-  <div className="columns wrap">
-    {imageUrls.map((image, index) => (
-      <div key={index} className="column is-one-quarter-desktop is-one-half-tablet">
-        <img src={image.imageUrl} />
-      </div>
-    ))}
-  </div>
-)
+class ImageGallery extends React.Component {
+	constructor(imageUrls) {
+		super(imageUrls);
+	
+		this.state = {
+		  modalIsOpen: false
+		};
+	
+		this.openModal = this.openModal.bind(this);
+		this.afterOpenModal = this.afterOpenModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	  }
+	
+	  openModal() {
+		this.setState({modalIsOpen: true});
+	  }
+	
+	  afterOpenModal() {
+		// references are now sync'd and can be accessed.
+		this.subtitle.style.color = '#f00';
+	  }
+	
+	  closeModal() {
+		this.setState({modalIsOpen: false});
+	  }
+	render() {
+		<div className="columns wrap">
+			{imageUrls.map((image, index) => (
+				<div key={index} className="column is-one-quarter-desktop is-one-half-tablet">
+					<a href='javascript:void(0)' onClick={this.openModal}><img src={image.imageUrl} /></a>
+					<Modal
+					isOpen={this.state.modalIsOpen}
+					onAfterOpen={this.afterOpenModal}
+					onRequestClose={this.closeModal}
+					style={customStyles}
+					contentLabel="Example Modal"
+					>
+						<img src={image.imageUrl} />
+					</Modal>
+				</div>
+			))}
+		</div>
+	}
+}
 
 ImageGallery.propTypes = {
   imageUrls: PropTypes.arrayOf(
