88c5d05e14651df6072fb0cd143ebfb468a5ce39
Patrick <estenpatrick@gmail.com>
2018-10-16 13:29:31 -0500

    should be working

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types'
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
-
+    console.log(data);
     return (
       <div className="hero-background-image full-height-plus-children">
             <div className="container hero-image-text has-text-grey-dark is-centered">
                 <div className="columns">
                     <div className="column welcome-pane">
-                        <h1>{data.markdownRemark.frontmatter.welcomeText}</h1>
+                        <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.welcomeText}</h1>
                         <hr />
-                        <p>{data.markdownRemark.frontmatter.aboutText}</p>
+                        <p>{data.allMarkdownRemark.edges[0].node.frontmatter.aboutText}</p>
                     </div>
                     <div className="column">
                         <img src="/img/draperVenetian.jpg" />
@@ -34,11 +34,15 @@ IndexPage.propTypes = {
 
 export const pageQuery = graphql`
   query IndexQuery {
-    markdownRemark {
-      frontmatter {
-        title
-        aboutText
-        welcomeText
+    allMarkdownRemark(filter: { fileAbsolutePath: {regex: "/src.pages.index.md/"} }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            aboutText
+            welcomeText
+          }
+        }
       }
     }
   }
