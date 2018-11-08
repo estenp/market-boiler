39acbc7aa95308f8fd76de127e9a8332ae125841
estenp <estenpatrick@gmail.com>
2018-11-07 21:26:45 -0600

    removing accidental merge miss

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,11 @@ import PropTypes from 'prop-types'
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
+    const { data } = this.props;
     console.log(data);
     return (
       <div className="hero-background-image full-height-plus-children">
             <div className="container hero-image-text has-text-grey-dark is-centered">
-
-                <div className="columns">
-                    <div className="column welcome-pane">
-                        <h1 className="centered">{data.allMarkdownRemark.edges[0].node.frontmatter.welcomeText}</h1>
-
                 <div className="side-by-side columns">
                     <div className="column text-pane">
                         <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.welcomeText}</h1>
