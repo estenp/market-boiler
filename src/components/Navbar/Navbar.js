0fbcf374017ad118824876bee3f56f44cf33138f
Esten Patrick <estenpatrick@gmail.com>
2019-01-16 08:59:01 -0600

    build working inherit component styles from global?

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,19 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
 
 const Navbar = ({ data }) => (
     <nav className="navbar is-transparent">
         <div className="container">
             <div className="navbar-brand">
                 <Link to="/" className="navbar-item">
-                    {/* <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure> */}
                     <div className="logo">
                         <img src="img/cfilogobluelight.svg" />
                     </div>
                 </Link>
             </div>
             <div className="navbar-end">
-                {/* <Link className="navbar-item" to="/about">
-          About
-        </Link> */}
+
                 <Link className="navbar-item" to="/vendors">
                     Vendors
                 </Link>
@@ -34,18 +27,6 @@ const Navbar = ({ data }) => (
                     Contact Us
                 </Link>
             </div>
-            {/* <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div> */}
         </div>
     </nav>
 );
