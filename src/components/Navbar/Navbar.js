f59a56ad025787920a5d8e1632130504ec2f2452
Esten Patrick <estenpatrick@gmail.com>
2019-01-16 21:13:48 -0600

    added/removed some plugins. need to update my template to styleName

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,27 +3,27 @@ import { Link } from "gatsby";
 import styles from "./Navbar.module.sass";
 
 const Navbar = ({ data }) => (
-    <nav className="{styles.navbar styles.is-transparent}">
-        <div className="{styles.container}">
-            <div className="{styles.navbar-brand}">
-                <Link to="/" className="{styles.navbar-item}">
-                    <div className="{styles.logo}">
+    <nav className={`${styles.navbar} ${styles.is-transparent}`}>
+        <div className={styles.container}>
+            <div className={styles.navbar-brand}>
+                <Link to="/" className={styles.navbar-item}>
+                    <div className={styles.logo}>
                         <img src="img/cfilogobluelight.svg" />
                     </div>
                 </Link>
             </div>
-            <div className="{styles.navbar-end}">
+            <div className={styles.navbar-end}>
 
-                <Link className="{styles.navbar-item}" to="/vendors">
+                <Link className={styles.navbar-item} to="/vendors">
                     Vendors
                 </Link>
-                <Link className="{styles.navbar-item}" to="/services">
+                <Link className={styles.navbar-item} to="/services">
                     Services
                 </Link>
-                <Link className="{styles.navbar-item}" to="/examples">
+                <Link className={styles.navbar-item} to="/examples">
                     Examples
                 </Link>
-                <Link className="{styles.navbar-item}" to="/contact">
+                <Link className={styles.navbar-item} to="/contact">
                     Contact Us
                 </Link>
             </div>
