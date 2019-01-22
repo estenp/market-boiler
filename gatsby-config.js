0c0e212355cf2baa69a84a7e87b3b616b3adddd9
Esten Patrick <estenpatrick@gmail.com>
2019-01-21 22:33:32 -0600

    global actually seems to be working. trying to figure out bulma imports

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,60 +1,64 @@
 module.exports = {
-    siteMetadata: {
-        title: "Market Boiler",
-    },
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        // {
-        //     resolve: `gatsby-plugin-postcss`,
-        //     options: {
-        //       postCssPlugins: [require(`postcss-preset-env`)({ stage: 2 })],
-        //     },
-        // },
-        "gatsby-plugin-sass",
-        {
-            resolve: `gatsby-plugin-react-css-modules`,
-            options: {
-              // *.css files are included by default.
-              // To support another syntax (e.g. SCSS),
-              // add `postcss-scss` to your project's devDependencies
-              // and add the following option here:
-              filetypes: {
-                ".scss": { syntax: `postcss-scss` }
-              },
-        
-              // Exclude global styles from the plugin using a RegExp:
-              exclude: `\/global\/`,
-              // For all the options check babel-plugin-react-css-modules README link provided above
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                path: `${__dirname}/src/pages`,
-                name: "pages",
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                path: `${__dirname}/src/img`,
-                name: "images",
-            },
-        },
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-        {
-            resolve: "gatsby-transformer-remark",
-            options: {
-                plugins: [],
-            },
-        },
-        {
-            resolve: "gatsby-plugin-netlify-cms",
-            options: {
-                modulePath: `${__dirname}/src/cms/cms.js`,
-            },
-        },
-        "gatsby-plugin-netlify", // make sure to keep it last in the array
-    ],
+	siteMetadata: {
+		title: "Market Boiler"
+	},
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		{
+			resolve: `gatsby-plugin-postcss`,
+			options: {
+				postCssPlugins: [
+					require(`postcss-preset-env`)({ stage: 2 }),
+					require(`postcss-modules`)
+				]
+			}
+		},
+		{
+			resolve: `gatsby-plugin-react-css-modules`,
+			options: {
+				// *.css files are included by default.
+				// To support another syntax (e.g. SCSS),
+				// add `postcss-scss` to your project's devDependencies
+				// and add the following option here:
+				filetypes: {
+					".scss": { syntax: `postcss-scss` }
+				},
+
+				// Exclude global styles from the plugin using a RegExp:
+				exclude: `\/global\/`
+				// For all the options check babel-plugin-react-css-modules README link provided above
+			}
+		},
+		"gatsby-plugin-sass",
+
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: "pages"
+			}
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/img`,
+				name: "images"
+			}
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: []
+			}
+		},
+		{
+			resolve: "gatsby-plugin-netlify-cms",
+			options: {
+				modulePath: `${__dirname}/src/cms/cms.js`
+			}
+		},
+		"gatsby-plugin-netlify" // make sure to keep it last in the array
+	]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,67 +2,67 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
-import "../global-styles/styles.scss";
+import "../global-styles/global-styles.module.scss";
 
 export default class IndexPage extends React.Component {
-    render() {
-        const { data } = this.props;
-        console.log(data);
-        return (
-            <Layout>
-                <div className="hero-background-image full-height-plus-children">
-                    <div className="container hero-image-text has-text-grey-dark is-centered">
-                        <div className="side-by-side columns">
-                            <div className="column text-pane">
-                                <h1>
-                                    {
-                                        data.allMarkdownRemark.edges[0].node
-                                            .frontmatter.welcomeText
-                                    }
-                                </h1>
+	render() {
+		const { data } = this.props;
+		console.log(data);
+		return (
+			<Layout>
+				<div className="hero-background-image full-height-plus-children">
+					<div className="container hero-image-text has-text-grey-dark is-centered">
+						<div className="side-by-side columns">
+							<div className="column text-pane">
+								<h1>
+									{
+										data.allMarkdownRemark.edges[0].node
+											.frontmatter.welcomeText
+									}
+								</h1>
 
-                                <hr />
-                                <p>
-                                    {
-                                        data.allMarkdownRemark.edges[0].node
-                                            .frontmatter.aboutText
-                                    }
-                                </p>
-                            </div>
-                            <div
-                                id="homepage-image"
-                                className="column image-pane"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </Layout>
-        );
-    }
+								<hr />
+								<p>
+									{
+										data.allMarkdownRemark.edges[0].node
+											.frontmatter.aboutText
+									}
+								</p>
+							</div>
+							<div
+								id="homepage-image"
+								className="column image-pane"
+							/>
+						</div>
+					</div>
+				</div>
+			</Layout>
+		);
+	}
 }
 
 IndexPage.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.shape({
-            frontmatter: PropTypes.object,
-        }),
-    }),
+	data: PropTypes.shape({
+		markdownRemark: PropTypes.shape({
+			frontmatter: PropTypes.object
+		})
+	})
 };
 
 export const pageQuery = graphql`
-    query IndexQuery {
-        allMarkdownRemark(
-            filter: { fileAbsolutePath: { regex: "/src.pages.index.md/" } }
-        ) {
-            edges {
-                node {
-                    frontmatter {
-                        title
-                        aboutText
-                        welcomeText
-                    }
-                }
-            }
-        }
-    }
+	query IndexQuery {
+		allMarkdownRemark(
+			filter: { fileAbsolutePath: { regex: "/src.pages.index.md/" } }
+		) {
+			edges {
+				node {
+					frontmatter {
+						title
+						aboutText
+						welcomeText
+					}
+				}
+			}
+		}
+	}
 `;
