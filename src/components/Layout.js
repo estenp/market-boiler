80e74cb00a0d24181565f77fff63103981e7d281
Esten Patrick <estenpatrick@gmail.com>
2019-01-28 21:48:00 -0600

    css modules working

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,34 +1,33 @@
 import React from "react";
 import Helmet from "react-helmet";
-import Layout from "../components/Layout";
 import { StaticQuery, graphql } from "gatsby";
 import Navbar from "../components/Navbar/Navbar";
 import StatusAlert from "../components/StatusAlert/StatusAlert";
 
 export default ({ children }) => (
-    <StaticQuery
-        query={graphql`
-            query StatusAlert {
-                markdownRemark(frontmatter: { type: { eq: "alert" } }) {
-                    html
-                    frontmatter {
-                        date(formatString: "MMMM DD, YYYY")
-                        path
-                        title
-                        type
-                    }
-                }
-            }
-        `}
-        render={data => (
-            <div>
-                <Helmet title="Custom Fabricating Industries" />
-                <Navbar />
-                {!!data && !!data.markdownRemark && (
-                    <StatusAlert message={data.markdownRemark.html} />
-                )}
-                <div className="main-container">{children}</div>
-            </div>
-        )}
-    />
+	<StaticQuery
+		query={graphql`
+			query StatusAlert {
+				markdownRemark(frontmatter: { type: { eq: "alert" } }) {
+					html
+					frontmatter {
+						date(formatString: "MMMM DD, YYYY")
+						path
+						title
+						type
+					}
+				}
+			}
+		`}
+		render={data => (
+			<div>
+				<Helmet title="Custom Fabricating Industries" />
+				<Navbar />
+				{!!data && !!data.markdownRemark && (
+					<StatusAlert message={data.markdownRemark.html} />
+				)}
+				<div className="main-container">{children}</div>
+			</div>
+		)}
+	/>
 );
