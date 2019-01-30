cb9c8c1c646b1044a6cc79c57bff1775c53e84ca
Esten Patrick <estenpatrick@gmail.com>
2019-01-29 22:45:14 -0600

    fixing a couple styles on nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,32 +3,47 @@ import { Link } from "gatsby";
 import "./Navbar.module.scss";
 
 const Navbar = ({ data }) => (
-    <nav styleName="navbar" className="navbar is-transparent">
-        <div className="container">
-            <div className="navbar-brand">
-                <Link to="/" className="navbar-item">
-                    <div className="logo">
-                        <img src="./img/cfilogobluelight.svg" />
-                    </div>
-                </Link>
-            </div>
-            <div className="navbar-end">
-
-                <Link className="navbar-item" to="/vendors">
-                    Vendors
-                </Link>
-                <Link className="navbar-item" to="/services">
-                    Services
-                </Link>
-                <Link className="navbar-item" to="/examples">
-                    Examples
-                </Link>
-                <Link className="navbar-item" to="/contact">
-                    Contact Us
-                </Link>
-            </div>
-        </div>
-    </nav>
+	<nav styleName="navbar" className="navbar is-transparent">
+		<div className="container">
+			<div className="navbar-brand">
+				<Link to="/" styleName="navbar-item" className="navbar-item">
+					<div className="logo">
+						<img src="../img/cfilogobluelight.svg" />
+					</div>
+				</Link>
+			</div>
+			<div className="navbar-end">
+				<Link
+					styleName="navbar-item"
+					className="navbar-item"
+					to="/vendors"
+				>
+					Vendors
+				</Link>
+				<Link
+					styleName="navbar-item"
+					className="navbar-item"
+					to="/services"
+				>
+					Services
+				</Link>
+				<Link
+					styleName="navbar-item"
+					className="navbar-item"
+					to="/examples"
+				>
+					Examples
+				</Link>
+				<Link
+					styleName="navbar-item"
+					className="navbar-item"
+					to="/contact"
+				>
+					Contact Us
+				</Link>
+			</div>
+		</div>
+	</nav>
 );
 
 export default Navbar;
