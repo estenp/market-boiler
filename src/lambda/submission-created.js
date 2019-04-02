c52cbff4627819b2b81a908a818e7759d438e3cf
Esten Patrick <estenpatrick@gmail.com>
2019-04-01 22:25:26 -0500

    netlify api call is working

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -1,25 +1,17 @@
 const axios = require("axios");
-const crypto = require("crypto");
-const token = process.env.netlify_access_token;
+//const crypto = require("crypto");
+const token = process.env.netlifyAPIToken;
 
-// exports.subs = async () => {
-// 	// await for results
-// 	//const res = await getFormSubmissions();
-// 	//console.log(res.data);
-// };
-
-exports.handler = function(event, context, callback) {
-	// fetch raw data from the randomuser api
-	const getFormSubmissions = () =>
-		axios
-			.get(`https://api.netlify.com/api/v1/forms/5c37534061fe65000800e763/submissions?access_token=${token}`)
-			.then(res => {
-				callback(null, {
-					statusCode: 200,
-					body: JSON.stringify(res.data)
-				});
-			})
-			.catch(err => {
-				callback(err);
+exports.handler = (event, context, callback) => {
+	axios
+		.get(`https://api.netlify.com/api/v1/forms/5c37534061fe65000800e763/submissions?access_token=${token}`)
+		.then(res => {
+			callback(null, {
+				statusCode: 200,
+				body: JSON.stringify(res.data)
 			});
+		})
+		.catch(err => {
+			callback(err);
+		});
 };
