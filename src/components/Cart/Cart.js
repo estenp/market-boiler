a1929a653148253bcf77b056f974da49d69fa361
Esten Patrick <estenpatrick@gmail.com>
2019-04-22 13:50:57 -0500

    updates to cart logic and styling

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,7 +4,20 @@ import "./Cart.module.scss";
 export default class Cart extends React.Component {
 	constructor(props) {
 		super(props);
-		console.log(props);
+
+		this.state = {
+			cartIsExpanded: true
+		};
+
+		this.toggleCart = this.toggleCart.bind(this);
+	}
+
+	toggleCart() {
+		if (this.props.cart.length > 0) {
+			this.setState({
+				cartIsExpanded: !this.state.cartIsExpanded
+			});
+		}
 	}
 
 	getProductNameByID(id) {}
@@ -14,20 +27,30 @@ export default class Cart extends React.Component {
 			<div styleName="cart-container">
 				<div className="card" styleName="cart">
 					<header className="card-header">
-						<p className="card-header-title">Cart</p>
-						<a href="#" className="card-header-icon" aria-label="more options">
+						<p className="card-header-title">
+							Cart &nbsp;&nbsp;<span>{this.props.cart.length}</span>
+						</p>
+
+						<a href="javascript:void(0)" className="card-header-icon" aria-label="expand-cart-items" onClick={this.toggleCart}>
 							<span className="icon">
-								<i className="fas fa-angle-down" aria-hidden="true" />
+								<i className={this.state.cartIsExpanded ? "fas fa-angle-up" : "fas fa-angle-down"} aria-hidden="true" />
 							</span>
 						</a>
 					</header>
-					<div className="card-content">
-						<ul>
-							{this.props.cart.map(prod => (
-								<li>{prod}</li>
-							))}
-						</ul>
-					</div>
+					{this.props.cart.length < 1 && (
+						<div className="card-content">
+							<p>Your cart is empty.</p>
+						</div>
+					)}
+					{this.props.cart.length > 0 && this.state.cartIsExpanded && (
+						<div className="card-content">
+							<ul>
+								{this.props.cart.map(prod => (
+									<li key={prod}>{prod}</li>
+								))}
+							</ul>
+						</div>
+					)}
 				</div>
 			</div>
 		);
