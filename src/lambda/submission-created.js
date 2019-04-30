7b3c56f1e1f69745e2abc7849e99791817f68c79
Esten Patrick <estenpatrick@gmail.com>
2019-04-30 09:55:35 -0500

    trying to update lambda

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -7,6 +7,7 @@ const pass = process.env.githubPass;
 
 exports.handler = (event, context, callback) => {
 	console.log(event);
+
 	axios({
 		method: "get",
 		//url: `https://api.github.com/repos/${user}/repo/market-boiler/src/data/orders/${event.form_id}`,
@@ -14,39 +15,33 @@ exports.handler = (event, context, callback) => {
 		headers: {
 			Authorization: `Bearer ${githubToken}`,
 			"Content-Type": "application/json"
-		},
-		auth: {
-			username: user,
-			password: pass
-		} /*,
-		data: {
-			message: "my commit message",
-			committer: {
-				name: "Esten",
-				email: "estenpatrick@gmail.com"
-			},
-			content: "bXkgbmV3IGZpbGUgY29udGVudHM="
-		}*/
+		}
 	})
-		.then(res => {
-			callback(null, {
+		.then(function() {
+			const response = {
 				statusCode: 200,
-				body: JSON.stringify(res.data)
-			});
+				headers: {
+					"Access-Control-Allow-Origin": "*", // Required for CORS support to work
+					"Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+				},
+				body: JSON.stringify({
+					message: "Successfully got repo info!"
+				})
+			};
+			callback(null, response);
 		})
-		.catch(err => {
-			callback(err);
+		.catch(e => {
+			const error = e.response.data;
+			const errorResponse = {
+				statusCode: error.status,
+				headers: {
+					"Access-Control-Allow-Origin": "*", // Required for CORS support to work
+					"Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+				},
+				body: JSON.stringify({
+					message: error.title
+				})
+			};
+			callback(null, errorResponse);
 		});
-	// axios
-	// 	// this is getting current form submissions for example.. will be making a post ? to add a submisson
-	// 	.get(`https://api.netlify.com/api/v1/forms/5c37534061fe65000800e763/submissions?access_token=${token}`)
-	// 	.then(res => {
-	// 		callback(null, {
-	// 			statusCode: 200,
-	// 			body: JSON.stringify(res.data)
-	// 		});
-	// 	})
-	// 	.catch(err => {
-	// 		callback(err);
-	// 	});
 };
