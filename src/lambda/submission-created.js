d3f8da917c9a79b3c9563590a1d6eb1f502e9743
Esten Patrick <estenpatrick@gmail.com>
2019-05-06 12:26:42 -0500

    last try before lunch

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -36,7 +36,8 @@ exports.handler = (event, context, callback) => {
 		}*/
 	})
 		.then(res => {
-			console.log(event);
+			console.log(event.body);
+			console.log(context);
 			callback(null, {
 				statusCode: 200,
 				headers: {
