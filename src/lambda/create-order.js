dd87db3f21d403ac1c676bafae31f0b24bdcc063
Esten Patrick <estenpatrick@gmail.com>
2019-06-21 23:52:30 -0500

    removing more unused lines

--- src/lambda/create-order.js
+++ src/lambda/create-order.js
@@ -1,8 +1,5 @@
 const axios = require("axios");
-//const fetch = require("node-fetch");
-//const crypto = require("crypto");
-const token = process.env.netlifyAPIToken;
-const githubToken = process.env.githubAPIToken;
+
 const user = process.env.githubUser;
 const pass = process.env.githubPass;
 
