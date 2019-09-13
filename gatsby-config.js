b7bbd3ad64b62d81ed137595c9b5fa2ddb7834ae
Esten Patrick <estenpatrick@gmail.com>
2019-09-13 13:12:50 -0500

    seems like dispensio is sort of working and so is studio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,24 @@
+require("dotenv").config({
+	path: `.env.${process.env.NODE_ENV}`
+});
+const {
+	api: {projectId, dataset}
+} = requireConfig("./studio/sanity.json");
+
+function requireConfig(path) {
+	try {
+		return require("./studio/sanity.json");
+	} catch (e) {
+		console.error("Failed to require sanity.json. Fill in projectId and dataset name manually in gatsby-config.js");
+		return {
+			api: {
+				projectId: process.env.SANITY_PROJECT_ID || "",
+				dataset: process.env.SANITY_DATASET || ""
+			}
+		};
+	}
+}
+
 module.exports = {
 	siteMetadata: {
 		title: "Market Boiler"
@@ -93,8 +114,8 @@ module.exports = {
 		{
 			resolve: "gatsby-source-sanity",
 			options: {
-				projectId: "<yourProjectId>",
-				dataset: "<yourDatasetName>",
+				projectId,
+				dataset,
 				// To enable preview of drafts, copy .env-example into .env,
 				// and add a token with read permissions
 				token: process.env.SANITY_TOKEN,
