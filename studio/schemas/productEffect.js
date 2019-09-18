712062ceb63d951640f54dab469869d5b4a08f91
Esten Patrick <estenpatrick@gmail.com>
2019-09-18 13:47:58 -0500

    adding productEffect schema

---
+++ studio/schemas/productEffect.js
@@ -0,0 +1,12 @@
+export default {
+	name: "productEffect",
+	title: "Product Effects",
+	type: "document",
+  fields: [
+    {
+      name: "label",
+      title: "Display Label",
+      type: "string"
+    }
+  ]
+}
